9bdade3f3be7bec352c4c9766aee8a26da063649
Rafael Schmitt <rafael.claycon@gmail.com>
2023-04-03 13:21:24 -0300

    Initial Commit

---
+++ AtividadeExpressaoRegular/AtividadeExpressaoRegularApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtividadeExpressaoRegularApp.swift
+//  AtividadeExpressaoRegular
+//
+//  Created by Rafael Schmitt on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AtividadeExpressaoRegularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
