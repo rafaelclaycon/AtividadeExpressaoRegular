bbd2c0cec91d9a27389e3d56e4301b81c2ab29ca
Rafael Schmitt <rafael.claycon@gmail.com>
2023-04-03 14:41:42 -0300

    Done

--- AtividadeExpressaoRegular/ValidationViewViewModel.swift
+++ AtividadeExpressaoRegular/ValidationViewViewModel.swift
@@ -21,18 +21,40 @@ class ValidationViewViewModel: ObservableObject {
     }
     
     func validateInput() {
-        if isInputValid() {
-            result = "OK!"
+        if currentMode == 0 {
+            result = isInputValid_RegularExpression() ? "OK!" : "Entrada inválida"
         } else {
-            result = "Entrada inválida"
+            result = isInputValid_Whitelist() ? "OK!" : "Entrada inválida"
         }
     }
     
-    func isInputValid() -> Bool {
+    func isInputValid_RegularExpression() -> Bool {
         guard !input.isEmpty else { return false }
         guard input.contains("/") else { return false }
         let range = NSRange(location: 0, length: input.utf16.count)
         let regex = try! NSRegularExpression(pattern: "[0-3]{1}[0-9]{1}\\/(0[1-9]|1[0-2])\\/[0-9]{4,4}$")
         return regex.firstMatch(in: input, options: [], range: range) != nil
+    }
+    
+    func isInputValid_Whitelist() -> Bool {
+        guard !input.isEmpty else { return false }
+        guard input.contains("/") else { return false }
+        guard input.count == 10 else { return false }
+        
+        let delimiter = "/"
+        let components = input.components(separatedBy: delimiter)
+        let day = components[0]
+        let month = components[1]
+        let year = components[2]
+        
+        guard day.count == 2 else { return false }
+        guard month.count == 2 else { return false }
+        guard year.count == 4 else { return false }
+        
+        guard let dayAsInt = Int(day), dayAsInt > 0, dayAsInt < 32 else { return false }
+        guard let monthAsInt = Int(month), monthAsInt > 0, monthAsInt < 13 else { return false }
+        guard let yearAsInt = Int(year), yearAsInt > 0, yearAsInt < 10000 else { return false }
+        
+        return true
     }
 }
--- AtividadeExpressaoRegularTests/AtividadeExpressaoRegularTests.swift
+++ AtividadeExpressaoRegularTests/AtividadeExpressaoRegularTests.swift
@@ -16,9 +16,167 @@ final class AtividadeExpressaoRegularTests: XCTestCase {
         sut = nil
     }
     
-    func test_validateInput_When31_12_2023_ShouldReturnOk() throws {
+    // MARK: - Regular expression
+    
+    func test_isInputValid_RegularExpression_WhenInputIs31_12_2023_ShouldReturnTrue() throws {
         sut = ValidationViewViewModel(input: "31/12/2023")
-        
-        XCTAssertTrue(sut.isInputValid())
+        XCTAssertTrue(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs12_01_2001_ShouldReturnTrue() throws {
+        sut = ValidationViewViewModel(input: "12/01/2001")
+        XCTAssertTrue(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs12_09_2022_ShouldReturnTrue() throws {
+        sut = ValidationViewViewModel(input: "12/09/2022")
+        XCTAssertTrue(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs12_09_20222_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "12/09/20222")
+        XCTAssertFalse(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs1_2_2003_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "1/2/2003")
+        XCTAssertFalse(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs01_02_03_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "01/02/03")
+        XCTAssertFalse(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs1_12_2023_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "1/12/2023")
+        XCTAssertFalse(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs12_25_2022_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "12/25/2022")
+        XCTAssertFalse(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs12_19_2022_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "12/19/2022")
+        XCTAssertFalse(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs12_29_2022_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "12/29/2022")
+        XCTAssertFalse(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs12_00_2022_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "12/00/2022")
+        XCTAssertFalse(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs41_12_2023_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "41/12/2023")
+        XCTAssertFalse(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs123_29_2022_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "123/29/2022")
+        XCTAssertFalse(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs123_293_2022_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "123/293/2022")
+        XCTAssertFalse(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs120320_0_2_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "120320/0/2")
+        XCTAssertFalse(sut.isInputValid_RegularExpression())
+    }
+    
+    func test_isInputValid_RegularExpression_WhenInputIs00_00_0000_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "00/00/0000")
+        XCTAssertFalse(sut.isInputValid_RegularExpression())
+    }
+    
+    // MARK: - Whitelist
+    
+    func test_isInputValid_Whitelist_WhenInputIs31_12_2023_ShouldReturnTrue() throws {
+        sut = ValidationViewViewModel(input: "31/12/2023")
+        XCTAssertTrue(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs12_01_2001_ShouldReturnTrue() throws {
+        sut = ValidationViewViewModel(input: "12/01/2001")
+        XCTAssertTrue(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs12_09_2022_ShouldReturnTrue() throws {
+        sut = ValidationViewViewModel(input: "12/09/2022")
+        XCTAssertTrue(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs12_09_20222_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "12/09/20222")
+        XCTAssertFalse(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs1_2_2003_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "1/2/2003")
+        XCTAssertFalse(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs01_02_03_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "01/02/03")
+        XCTAssertFalse(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs1_12_2023_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "1/12/2023")
+        XCTAssertFalse(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs12_25_2022_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "12/25/2022")
+        XCTAssertFalse(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs12_19_2022_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "12/19/2022")
+        XCTAssertFalse(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs12_29_2022_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "12/29/2022")
+        XCTAssertFalse(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs12_00_2022_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "12/00/2022")
+        XCTAssertFalse(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs41_12_2023_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "41/12/2023")
+        XCTAssertFalse(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs123_29_2022_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "123/29/2022")
+        XCTAssertFalse(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs123_293_2022_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "123/293/2022")
+        XCTAssertFalse(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs120320_0_2_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "120320/0/2")
+        XCTAssertFalse(sut.isInputValid_Whitelist())
+    }
+    
+    func test_isInputValid_Whitelist_WhenInputIs00_00_0000_ShouldReturnFalse() throws {
+        sut = ValidationViewViewModel(input: "00/00/0000")
+        XCTAssertFalse(sut.isInputValid_Whitelist())
     }
 }
